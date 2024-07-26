bc7d5d10c9b1e331fb9f93c16ea0ea2b5d5ab3b6
amadii69 <amadii69@gmail.com>
2024-07-26 18:04:04 +0200

    commit Ec2 with webserver

--- main.tf
+++ main.tf
@@ -5,8 +5,26 @@ provider "aws" {
 resource "aws_instance" "up-and-run-example" {
   ami           = "ami-07c8c1b18ca66bb07" # The valid AMI ID obtained
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = <<-EOF
+  	#!/bin/bash
+  	echo "Hello, World" > index.xhtml
+  	nohup busybox httpd -f -p 8080 &
+  	EOF
+  user_data_replace_on_change = true
 
   tags = {
     Name = "example-instance"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }  
+
 }
